42cd00f601b24101ea9b9fb62f75666998277cfc
aksyuk <s.a.aksuk@gmail.com>
2019-10-01 19:22:00 +0300

    convert_to_pdf.R created

---
+++ RScripts/practice/convert_to_pdf.R
@@ -0,0 +1,16 @@
+
+# source: https://github.com/yihui/xaringan/wiki/Export-Slides-to-PDF
+
+# install.packages(c("pagedown", "xaringan"))
+# # make sure you have pagedown >= 0.2 and xaringan >= 0.9; if not, run
+# # remotes::install_github(c('rstudio/pagedown', 'yihui/xaringan'))
+
+library('pagedown')
+library('xaringan')
+
+pagedown::chrome_print("slides_practice_03.Rmd")
+
+# # or just pass the HTML output file path to chrome_print()
+# pagedown::chrome_print("path/to/your/slides.html")
+
+
